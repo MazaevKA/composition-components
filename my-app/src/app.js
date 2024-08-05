a716d30630a900177f11ec00ccd601a96c94ffef
MazaevKA <mazaj.ru@mail.ru>
2024-08-05 19:23:38 +0500

    tasks ready

--- my-app/src/app.js
+++ my-app/src/app.js
@@ -1,25 +1,10 @@
-import {User, H3} from './componets'
-import styles from './app.module.css';
+import PropTypes from "prop-types";
 
-const getUserFromServer = () => ({
-  name: 'Иван',
-  age: 25,
-  email: 'ivan@mail.ru',
-  phone: '+7(999)-999-99-99',
-})
+const Product = ({name, price}) => {
+  return <div>{name} - {price} руб.</div>
+}
 
-export const App = () => {
-  const user = getUserFromServer();
-	return (
-		<div className={styles.app}>
-      <H3 color={'red'}>Приложение:</H3>
-      <div>Разная информация приложения</div>
-      {/* Передача props */}
-      {/*<User name={user.name} age={user.age} email={user.email} phone={user.phone} />*/}
-
-      {/* Передача props через спред оператор */}
-      <User {...user} />
-
-		</div>
-	);
+Product.propTypes = {
+  name: PropTypes.string,
+  price: PropTypes.number,
 };
