e3845fd31cd94c8c6ef1fe91c3796245d0344d32
MazaevKA <mazaj.ru@mail.ru>
2024-08-06 14:12:22 +0500

    tasks ready

--- my-app/src/app.js
+++ my-app/src/app.js
@@ -1,7 +1,18 @@
 import PropTypes from "prop-types";
+import {useState} from "react";
 
-const Product = ({name, price}) => {
-  return <div>{name} - {price} руб.</div>
+const Product = ({name, price, amount}) => {
+  // const [amount, setAmount] useState(1); переехало к родительскому компоненту
+  return (
+    <>
+      <div>
+        {name} - {price} руб
+      </div>
+      <div>Количество: {amount}</div>
+      <Basket amount={amount} />
+    </>
+  )
+
 }
 
 Product.propTypes = {
