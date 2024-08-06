18858f0d3ba8f5560d6246f76d770f569bdc3435
MazaevKA <mazaj.ru@mail.ru>
2024-08-06 18:46:49 +0500

    tasks ready

--- my-app/src/app.js
+++ my-app/src/app.js
@@ -5,7 +5,7 @@ const Counter = ({value, setValue}) => {
   return (
     <>
       <div>{value}</div>
-      <button onClick={() => setValue(value + 1)}>+ 1</button>
+      <button onClick={() => setValue(value + 1)}>{value} + 1</button>
     </>
   )
 }
@@ -13,8 +13,8 @@ export const App = () => {
   const [value, setValue] = useState(0);
 
   return (
-      <div className={StyleSheet.app}>
-        <label>Счёткчик:</label>
+      <div className={styles.app}>
+        <label>Счёткчик: {value}</label>
         <Counter value={value} setValue={setValue} />
       </div>
   )
