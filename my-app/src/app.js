4791be979ad211e7b02bee43ca2d524d1ceb60f6
MazaevKA <mazaj.ru@mail.ru>
2024-08-06 20:15:06 +0500

    tasks ready

--- my-app/src/app.js
+++ my-app/src/app.js
@@ -1,9 +1,21 @@
 import styles from './app.module.css';
+import {useState} from "react";
+
+const AppLayout = ({a, b, setA, setB, sum}) => (
+  <div className={styles.app}>
+    <div>A: {a}</div>
+    <button onClick={() => setA(a + 1)}>Прибавить 1 к A</button>
+    <div>B: {b}</div>
+    <button onClick={() => setB(b + 1)}>Прибавить 1 к B</button>
+    <div>Сумма A + B: {sum}</div>
+  </div>
+);
 
 export const App = () => {
-	return (
-		<div className={styles.app}>
-      <div></div>
-		</div>
-	);
+  const [a, setA] = useState(0);
+  const [b, setB] = useState(0);
+
+  const sum = a + b;
+
+  return <AppLayout a={a} b={b} setA={setA} setB={setB} sum={sum} />
 };
